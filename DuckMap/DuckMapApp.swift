111a4e278c460634d90c91ca2bdc0c51cd4d09d8
Florent Fougères <florent.fougeres@gmail.com>
2026-06-22 19:38:38 +0200

    Initial Commit

---
+++ DuckMap/DuckMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuckMapApp.swift
+//  DuckMap
+//
+//  Created by Florent on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct DuckMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
